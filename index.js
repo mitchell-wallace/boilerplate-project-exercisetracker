7a56b812e2406de1a8cdb563b3045048e6756340
Mitchell Wallace <38238516+mitchell-wallace@users.noreply.github.com>
2023-12-10 21:14:28 +1100

    POST /api/users working

--- index.js
+++ index.js
@@ -2,16 +2,35 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser');
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
+
+const exercises = [];
+  // String username
+  // String description
+  // Number duration
+  // Date date
+  // String _id
+
+const users = [];
+  // String username
+  // String _id
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+app.post('/api/users', (req, res) => {
+  var newUser = {
+    'username': req.body.username,
+    '_id': users.length
+    }
+  users.push(newUser);
+  res.json(newUser);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
