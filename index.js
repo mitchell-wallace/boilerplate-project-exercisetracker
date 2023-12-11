90d0f1fdcb51e271088615fce4306edd21e613c2
Mitchell Wallace <38238516+mitchell-wallace@users.noreply.github.com>
2023-12-11 14:09:10 +1100

    WIP exercises returns a response
    also users endpoints now correctly do not return log values

--- index.js
+++ index.js
@@ -25,39 +25,52 @@ app.get('/', (req, res) => {
 });
 
 // creates 1 new user and returns this user's username and _id
-// TODO: hide log in this
+// the user will not have a log at the point of creation
 app.post('/api/users', (req, res) => {
   var newUser = {
     'username': req.body.username,
-    '_id': users.length,
-    'log': []
+    '_id': users.length
     }
   users.push(newUser);
   res.json(newUser);
 });
 
 // returns all users in array
-// TODO: hide log in these
+// TODO: hide log in these --> I think I've fixed this using Poe Assistant
 app.get('/api/users', (req, res) => {
-  res.json(users);
+  
+  const selectedAttributes = ["username", "_id"];
+
+  const result = users.map(obj => {
+    const newObj = {};
+    selectedAttributes.forEach(attr => {
+      newObj[attr] = obj[attr];
+    });
+    return newObj;
+  });
+
+  res.json(result);
 });
 
 // creates 1 new exercise for user matching _id
 app.post('/api/users/:_id/exercises', (req, res) => {
-  // searching for the id because assuming it is the index is boring
+  // searching for id so that we can use identifiers other than the index
   var userIndex;
+  // var idInput = req.params._id;
+  // var targetId = Number.parseInt(idInput);
   for (var i = 0; i < users.length; i++) {
-    if (users[i].id = req.body._id) {
+    if (users[i]._id == req.params._id) {
       userIndex = i;
     }
   }
 
+  // set up variables for new entry
   var description = req.body.description;
   var duration = parseInt(req.body.duration);
   var date;
   if (!req.body.date) date = new Date().toDateString();
   else date = new Date(req.body.date).toDateString();
-  // date should be like "Sun Dec 10 2023"
+    // date should be like "Sun Dec 10 2023"
 
   var newExercise = {
     'description': description,
@@ -65,6 +78,12 @@ app.post('/api/users/:_id/exercises', (req, res) => {
     'date': date
   };
 
+  // if user has no log yet, create one
+  // try {!users[userIndex].log;}
+  // catch {users[userIndex].log = [];}
+  if (!users[userIndex].log) users[userIndex].log = [];
+
+  // add exercise to user's log and return
   users[userIndex].log.push(newExercise);
   res.json(users[userIndex]);
 });
